827a293b90919b79635c92da07858c0311c87476
moskin.sy <sergejmoskin94@mail.ru>
2018-04-02 15:39:57 +0300

    Lab4

--- Lab4.py
+++ Lab4.py
@@ -1,15 +1,44 @@
 # Лабораторная работа №4
 # Массивы(списки)
 
-n = int(input('Укажите размер массива:'))
-mas = []
-i = 0
+from math import cosh, log
 
-for i in range(n):
-    x = int(input('Введите {} элемент массива'.format(i+1)))
-    mas.append(x)
-y = int(input('Выберите элемент массива:'))
+mas_in = []
 
-if y <= n:
-    print('Данный элемент = {}'.format(mas[y-1]))
-print('Данный элемент отсутствует в массиве')
+mas_in.append(input('Enter x:'))
+mas_in.append(input('Enter a:'))
+
+x = float(mas_in[0])
+a = float(mas_in[1])
+
+while ((3 * a ** 2 - 25 * a * x + 8 * x ** 2 + 1) / log(10)) < 0:
+    print('Невозможно рассчитать функцию Y с данными значениями переменных')
+
+    mas_in.clear()
+    mas_in.append(input('Enter x:'))
+    mas_in.append(input('Enter a:'))
+
+    x = float(mas_in[0])
+    a = float(mas_in[1])
+
+while ((21 * a ** 2 - 34 * a * x + 9 * x ** 2) < -710.000001) or ((21 * a ** 2 - 34 * a * x + 9 * x ** 2) > 710.000001):
+    print('Невозможно рассчитать функцию F с данными значениями переменных')
+
+    mas_in.clear()
+    mas_in.append(input('Enter x:'))
+    mas_in.append(input('Enter a:'))
+
+    x = float(mas_in[0])
+    a = float(mas_in[1])
+
+g = -(10 * (18 * a ** 2 + 11 * a * x - 24 * x ** 2) / (-a ** 2 + a * x + 6 * x ** 2))
+y = log(3 * a ** 2 - 25 * a * x + 8 * x ** 2 + 1) / log(10)
+f = cosh(21 * a ** 2 - 34 * a * x + 9 * x ** 2)
+
+mas_out = []
+mas_out.append('G={}'.format(g))
+mas_out.append('Y={}'.format(y))
+mas_out.append('F={}'.format(f))
+
+for i in range(len(mas_out)):
+    print(mas_out[i])
