551b97346555fdb6d229e94c782dd01ca7cdf786
PPOEVT\moskin.sy <sergejmoskin@gmail.com>
2018-05-11 13:57:16 +0300

    no message

--- Lab6.py
+++ Lab6.py
@@ -2,8 +2,12 @@
 
 from math import cosh, log
 
-class Print:
 
+class Out(object):
+    def __init__(self, name=None,  value=2000):
+        self.name = name
+        self.value = value
+        print(name, value)
 
 
 x = float(input('Enter x:'))
@@ -19,13 +23,7 @@ while ((21 * a ** 2 - 34 * a * x + 9 * x ** 2) < -710.000001) or ((21 * a ** 2 -
     x = float(input('Enter x:'))
     a = float(input('Enter a:'))
 
-g = -(10 * (18 * a ** 2 + 11 * a * x - 24 * x ** 2) / (-a ** 2 + a * x + 6 * x ** 2))
-y = log(3 * a ** 2 - 25 * a * x + 8 * x ** 2 + 1) / log(10)
-f = cosh(21 * a ** 2 - 34 * a * x + 9 * x ** 2)
 
-
-
-print('G={}'. format(g))
-print('Y={}'. format(y))
-print('F={}'. format(f))
-
+g = Out('g =', -(10 * (18 * a ** 2 + 11 * a * x - 24 * x ** 2) / (-a ** 2 + a * x + 6 * x ** 2)))
+y = Out('y =', log(3 * a ** 2 - 25 * a * x + 8 * x ** 2 + 1) / log(10))
+f = Out('f =', cosh(21 * a ** 2 - 34 * a * x + 9 * x ** 2))
