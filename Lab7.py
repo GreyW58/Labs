4da90348da597dd7a9d2ba8f5cb4f2b97b09f7fb
DESKTOP-2TVC1UA\GreyW <sergejmoskin94@mail.ru>
2018-05-13 20:43:44 +0300

    no message

--- Lab7.py
+++ Lab7.py
@@ -2,13 +2,14 @@
 
 from math import cosh, log
 
-in_f = open('in.txt')
-value = in_f.read()
-len = len(value)
-print(len)
+class In(object):
 
-x = float(input('Enter x:'))
-a = float(input('Enter a:'))
+    in_f = open('in.txt')
+    value = [line.strip() for line in in_f.readlines()]
+
+x = float(In.value[0])
+a = float(In.value[1])
+
 
 while ((3 * a ** 2 - 25 * a * x + 8 * x ** 2 + 1) / log(10)) < 0:
     print('Невозможно рассчитать функцию Y с данными значениями переменных')
@@ -24,7 +25,8 @@ g = -(10 * (18 * a ** 2 + 11 * a * x - 24 * x ** 2) / (-a ** 2 + a * x + 6 * x *
 y = log(3 * a ** 2 - 25 * a * x + 8 * x ** 2 + 1) / log(10)
 f = cosh(21 * a ** 2 - 34 * a * x + 9 * x ** 2)
 
-print('G={}'. format(g))
-print('Y={}'. format(y))
-print('F={}'. format(f))
-
+out_f = open('out.txt', 'w')
+out = str('G={} \n'.format(g) + 'Y={} \n'.format(y) + 'F={} \n'.format(f))
+for index in out:
+    out_f.write(index)
+out_f.close()
